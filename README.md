c4af8a5c475bb2b3aa5acce223c83cde3ad2fa43
DevJosimar <josimarfsjunior@gmail.com>
2026-07-09 19:54:07 -0300

    1- Melhoria realizada no frontEnd colocando a classificação visual por cor, também criei uma escala que é preenchida de acordo com a IMC calculado; 2- Melhoria realizada na parte de estilização; 3- Melhoria realizada no backEnd criando regra para caso o usuário não preencha os campos de peso e altura, melhoria realizada para limpar os campos preenchidos anteriormente sempre que realizado um novo calculo.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # CALCULADORA DE IMC - ÍNDICE DE MASSA CORPORAL
-Projeto criado com objetivo de criar uma calculadora de IMC - Índice de Massa Corporal.
+
+Projeto criado com objetivo de construir uma calculadora de IMC - Índice de Massa Corporal.
+
+## Regras Criadas:
+
+### Campos Obrigatórios
+
+1 - Peso em Kg: (Exemplo: 65);
+
+2 - Altura em Mt: (Exemplo: 1.60);
+
+### Função de Calcular IMC
+
+1 - Ao ser acionada, executa a regra de calculo construída com base no 'Peso' e a 'Altura' informada pelo usuário.
+
+2- Os campos e valores são limpados da tela a cada novo cálculo realizado pelo usuário.
+
+3- Além de apresentar o resultado para o usuário é apresentado o IMC calculado em uma escala visual.
+
+## Experiência alcançada
+
+Desenvolvi essa Calculadora de IMC utilizando HTML, CSS e JavaScript, aplicando conceitos de estruturação de páginas, estilização, manipulação do DOM, eventos e lógica de programação. O projeto fortaleceu minha compreensão sobre o desenvolvimento front-end e a criação de interfaces funcionais.
+
+Além disso, aprendi a utilizar o Git e o GitHub para versionamento de código, dominando comandos essenciais como inicialização de repositório, criação de commits, configuração de repositório remoto e publicação do projeto diretamente no GitHub, tornando meu fluxo de desenvolvimento mais organizado e profissional.
--- backEnd/script.js
+++ backEnd/script.js
@@ -16,6 +16,14 @@ var altura;
 var imcCalculado;
 var resultado;
 
+var posicaoIMC;
+var posicaoIMCBaixo;
+var posicaoIMCNormal;
+var posicaoIMCSobrePeso;
+var posicaoIMCObesidade1;
+var posicaoIMCObesidade2;
+var posicaoIMCObesidade3;
+
 function calcularIMC(event) {
   //preventDefault =  Impede o recarregamento da página
   event.preventDefault();
@@ -27,19 +35,95 @@ function calcularIMC(event) {
   imcCalculado = peso / (altura * altura);
 
   resultado = document.getElementById("resultado");
+  posicaoIMCBaixo = document.getElementById("linhaBaixo");
+  posicaoIMCNormal = document.getElementById("linhaNormal");
+  posicaoIMCSobrePeso = document.getElementById("linhaSobrepeso");
+  posicaoIMCObesidade1 = document.getElementById("linhaSobrepesoI");
+  posicaoIMCObesidade2 = document.getElementById("linhaSobrepesoII");
+  posicaoIMCObesidade3 = document.getElementById("linhaSobrepesoIII");
+
+  /**Regra criada para informar o usuário da obrigatoriedade de preencher os campos */
+  if (peso === null || (peso === " " && altura === null) || altura === "") {
+    alert(
+      "Para realizar o calculo de IMC é necessário informar o Peso e a Altura!",
+    );
+  }
 
   if (imcCalculado < 18.5) {
     resultado.innerHTML = `<br/> Seu IMC é de ${imcCalculado.toFixed(2)}, CLASSIFICAÇÃO: Peso baixo!`;
+    posicaoIMCBaixo.innerHTML = `${imcCalculado.toFixed(2)}`;
+
+    //posicaoIMCBaixo.innerHTML = "";
+    posicaoIMCNormal.innerHTML = "";
+    posicaoIMCSobrePeso.innerHTML = "";
+    posicaoIMCObesidade1.innerHTML = "";
+    posicaoIMCObesidade2.innerHTML = "";
+    posicaoIMCObesidade3.innerHTML = "";
   } else if (imcCalculado > 18.5 && imcCalculado < 24.9) {
     resultado.innerHTML = `<br/> Seu IMC é de ${imcCalculado.toFixed(2)}, CLASSIFICAÇÃO: Peso normal`;
+    posicaoIMCNormal.innerHTML = `${imcCalculado.toFixed(2)}`;
+
+    /**Criado para garantir que os demais campos fiquem limpos após usuário
+     * realizar novo calculo.
+     */
+    posicaoIMCBaixo.innerHTML = "";
+    //posicaoIMCNormal.innerHTML = "";
+    posicaoIMCSobrePeso.innerHTML = "";
+    posicaoIMCObesidade1.innerHTML = "";
+    posicaoIMCObesidade2.innerHTML = "";
+    posicaoIMCObesidade3.innerHTML = "";
   } else if (imcCalculado > 25 && imcCalculado < 29.9) {
     resultado.innerHTML = `<br/> Seu IMC é de ${imcCalculado.toFixed(2)}, CLASSIFICAÇÃO: Sobrepeso`;
+    posicaoIMCSobrePeso.innerHTML = `${imcCalculado.toFixed(2)}`;
+
+    /**Criado para garantir que os demais campos fiquem limpos após usuário
+     * realizar novo calculo.
+     */
+    posicaoIMCBaixo.innerHTML = "";
+    posicaoIMCNormal.innerHTML = "";
+    //posicaoIMCSobrePeso.innerHTML = '';
+    posicaoIMCObesidade1.innerHTML = "";
+    posicaoIMCObesidade2.innerHTML = "";
+    posicaoIMCObesidade3.innerHTML = "";
   } else if (imcCalculado > 30 && imcCalculado < 34.9) {
     resultado.innerHTML = `<br/> Seu IMC é de ${imcCalculado.toFixed(2)}, CLASSIFICAÇÃO: Obesidade I`;
+    posicaoIMCObesidade1.innerHTML = `${imcCalculado.toFixed(2)}`;
+
+    /**Criado para garantir que os demais campos fiquem limpos após usuário
+     * realizar novo calculo.
+     */
+    posicaoIMCBaixo.innerHTML = "";
+    posicaoIMCNormal.innerHTML = "";
+    posicaoIMCSobrePeso.innerHTML = "";
+    //posicaoIMCObesidade1.innerHTML = '';
+    posicaoIMCObesidade2.innerHTML = "";
+    posicaoIMCObesidade3.innerHTML = "";
   } else if (imcCalculado > 35 && imcCalculado < 39.9) {
     resultado.innerHTML = `<br/> Seu IMC é de ${imcCalculado.toFixed(2)}, CLASSIFICAÇÃO: Obesidade II`;
+    posicaoIMCObesidade2.innerHTML = `${imcCalculado.toFixed(2)}`;
+
+    /**Criado para garantir que os demais campos fiquem limpos após usuário
+     * realizar novo calculo.
+     */
+    posicaoIMCBaixo.innerHTML = "";
+    posicaoIMCNormal.innerHTML = "";
+    posicaoIMCSobrePeso.innerHTML = "";
+    posicaoIMCObesidade1.innerHTML = "";
+    //posicaoIMCObesidade2.innerHTML = '';
+    posicaoIMCObesidade3.innerHTML = "";
   } else if (imcCalculado > 40) {
     resultado.innerHTML = `<br/> Seu IMC é de ${imcCalculado.toFixed(2)}, CLASSIFICAÇÃO: Obesidade III`;
+    posicaoIMCObesidade3.innerHTML = `${imcCalculado.toFixed(2)}`;
+
+    /**Criado para garantir que os demais campos fiquem limpos após usuário
+     * realizar novo calculo.
+     */
+    posicaoIMCBaixo.innerHTML = "";
+    posicaoIMCNormal.innerHTML = "";
+    posicaoIMCSobrePeso.innerHTML = "";
+    posicaoIMCObesidade1.innerHTML = "";
+    posicaoIMCObesidade2.innerHTML = "";
+    //posicaoIMCObesidade3.innerHTML = '';
   }
 
   //Limpando o formulário
